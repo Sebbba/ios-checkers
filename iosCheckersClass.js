2a0a7322c6eea48c69dbec4f2dbd183f59909bdc
Sebbba <dsiebov@gmail.com>
2015-11-16 20:17:28 +0200

    iosCheckersClass v 1.0

--- iosCheckersClass.js
+++ iosCheckersClass.js
@@ -1,7 +1,37 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function iosCheckersClass() {
+    this.render = function(jQuerySelector) {
+        var $elems = $(jQuerySelector);
+        
+        var make = function(){
+            $elems.each(function(index,elem) {
+                var $elem = $(elem);
+                var $inp = $('<input type="hidden" value="0">');
+                    if($elem.attr("name")){
+                        $inp.attr("name",$elem.attr("name"));}
+                var $check = $('<div>');
+                    $check.attr('class','checkbox');
+                var elem_attr = $elem.prop("attributes");
+                    $.each(elem_attr, function() {
+                        if(this.name !== "id" || this.name !== "type"){
+                            $check.attr(this.name, this.value);
+                        }
+                    });
+
+                $check.wrap($inp);
+                    $check.on('click',function(event) {
+                        if($(this).hasClass('checked')){
+                            $(this).removeClass('checked');
+                            $inp.attr("value",0);
+                        }else {
+                            $(this).addClass('checked');
+                            $inp.attr("value",1);
+                        }
+                    });
+                $(this).replaceWith($check);
+                });
+        }
+        return $elems.each(make);
+    }
+}
 
 
