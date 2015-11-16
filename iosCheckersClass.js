3e82d05cd92f0cb01bebdc20dd478224e57d491f
Sebbba <dsiebov@gmail.com>
2015-11-16 22:54:43 +0200

    code Refactoring

--- iosCheckersClass.js
+++ iosCheckersClass.js
@@ -1,26 +1,29 @@
-var iosCheckersClass =  function() {};
+var iosCheckersCreator =  function() {};
 
-iosCheckersClass.prototype.render = function(jQuerySelector) {
-        var $elems = $(jQuerySelector);
-        
+iosCheckersCreator.prototype.render = function(jQuerySelector) {
+    var $elems = $(jQuerySelector);
+        //
         var make = function(){
             $elems.each(function(index,elem) {
                 var $elem = $(elem);
+                //
                 var $inp = $('<input type="hidden" value="0">');
                     if($elem.attr("name")){
-                        $inp.attr("name",$elem.attr("name"));}
+                        $inp.attr("name",$elem.attr("name"));
+                    }
                 var $check = $('<div>');
                     $check.attr('class','checkbox');
+                //
                 var elem_attr = $elem.prop("attributes");
                     $.each(elem_attr, function() {
                         if(this.name !== "id" || this.name !== "type"){
                             $check.attr(this.name, this.value);
                         }
                     });
-
                 $check.wrap($inp);
+                //
                     $check.on('click',function(event) {
-                        $checker = $(this)
+                        $checker = $(this);
                         if($checker.hasClass('checked')){
                             $checker.removeClass('checked');
                             $inp.attr("value",0);
@@ -29,9 +32,9 @@ iosCheckersClass.prototype.render = function(jQuerySelector) {
                             $inp.attr("value",1);
                         }
                     $checker.trigger('change', $inp.val());
-                });
+                    });
                 $(this).replaceWith($check);
-                });
+            });
         }
         return $elems.each(make);
 }
